48f99e718c4bc3b7d269327094b0e36b28096ed2
Enric Climent <enric@ecliment.ovh>
2015-06-09 09:58:09 +0200

    update

--- internetbbb.sh
+++ internetbbb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#eth0 is my internet facing interface, eth1 is the BeagleBone USB connection
-echo Configurant ip eth1
-sudo ifconfig eth1 192.168.7.1
+#eth0 is my internet facing interface, eth2 is the BeagleBone USB connection
+echo Configurant ip eth2
+sudo ifconfig eth2 192.168.7.1
 sudo iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
-sudo iptables --append FORWARD --in-interface eth1 -j ACCEPT
+sudo iptables --append FORWARD --in-interface eth2 -j ACCEPT
 sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 echo Fet
